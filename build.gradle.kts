5e8593d66e0c7508cebdd1162f8d425d19fd1188
Diego Guido <dguidor@gmail.com>
2025-06-04 12:22:54 -0600

    sonarqube config and fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.sonar.qube)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "guido5_FirebaseAuthAndroid")
+        property("sonar.organization", "guido5")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
